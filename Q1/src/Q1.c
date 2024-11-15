28ff10b6e4f072b593e1a71b8df7c7c5316bac35
IagoraNz <iago.almeida@ufpi.edu.br>
2024-11-15 14:33:36 -0300

    Refactor 2-3 tree implementation: update node structure and insertion logic

--- Q1/src/Q1.c
+++ Q1/src/Q1.c
@@ -14,68 +14,68 @@
 
 /* (pré-itens) funções necessárias para que os itens i, ii, iii e iv possam ocorrer */
 
-/*
-    summary
-        Função para inserir uma palavra em uma árvore 2-3
-    parameters
-        Arv23pt **no: nó da árvore 2-3
-        char *word: palavra a ser inserida
-        char *promove: palavra a ser promovida
-        Arv23pt **pai: nó pai da árvore 2-3
-*/
-Arv23pt *insere(Arv23pt **no, char *word, char *promove, Arv23pt **pai){
-    Arv23pt *maiorno;
-    int promove1;
-    maiorno = NULL;
+Arv23PT *criaNo(Info informacao, Arv23PT *filhoesq, Arv23PT *filhocen){
+    Arv23PT *no;
+    no = (Arv23PT*)malloc(sizeof(Arv23PT));
 
-    // Verifica se o nó é nulo, se for, cria um novo nó
-    if(*no = NULL)
-        *no = criaNo(word, NULL, NULL);
-    else{ // Caso contrário, insere a palavra no nó
-        if(ehFolha(*no)){ // Verifica se o nó é folha
-            if((*no)->ninfos == 1) // Verifica se o nó possui apenas uma informação, ou seja, possui um slot disponível
-                *no = adicionaChave(*no, word, NULL);
-            else{ 
-            /*
-            Se o nó possuir duas informações, quebra o nó e promove a palavra para o nó pai
-            de modo que o nó pai possua uma informação e os filhos possuam uma informação cada
-            */
-                maiorno = quebraNo(no, word, promove, NULL);
+    no->info1 = informacao;
+    no->esq = filhoesq;
+    no->cen = filhocen;
+    no->ninfos = 1;
+
+    return no;
+}
+
+int ehFolha(Arv23PT *no){
+    /* Basta verificar se a esquerda é NULL */
+    return (no->esq == NULL) ? 1 : 0;
+}
+
+Arv23PT *inserirArv23(Arv23PT **no, Info informacao, Info *promove, Arv23PT **pai){
+    Info promove1;
+    Arv23PT *maiorNo;
+    maiorNo = NULL;
+
+    if(*no == NULL)
+        *no = criaNo(informacao, NULL, NULL);
+    else{
+        if(ehfolha(*no)){
+            if((*no)->ninfos == 1)
+                *no = adicionaChave(*no, informacao, NULL);
+            else{
+                maiorNo = quebraNo(no, informacao, promove, NULL);
                 if(*pai == NULL){
-                    *no = criaNo(*promove, *no, maiorno);
-                    maiorno = NULL;
+                    *no = criaNo(*promove, *no, maiorNo);
+                    maiorNo == NULL;
                 }
             }
         }
-        else{ // Caso contrário, insere a palavra no nó filho
-            /*
-            1. Verifica se a palavra é menor que a palavra1 do nó, se for, insere a palavra no nó esquerdo
-            2. Verifica se há apenas uma informação no nó ou se a palavra é maior que a palavra1 do nó, 
-            se for, insere a palavra no nó do meio  
-            3. Caso contrário, insere a palavra no nó direito
-            */
-            if(strcmp(word, (*no)->word) < 0) 
-                maiorno = insere(&(*no)->esq, word, promove, no);
-            else if(((*no)->ninfos == 1) || (strcmp(word, (*no)->word) > 0))
-                maiorno = insere(&(*no)->meio, word, promove, no);
-            else
-                maiorno = insere(&(*no)->dir, word, promove, no);
+        else{
+            if(strcmp(informacao->palavra, (*no)->info1.palavra) < 0){
+                maiorNo = inserirArv23(&((*no)->esq), informacao, promove, no);
+            }
+            else if((*no)->ninfos == 1 || (strcmp(informacao->palavra, (*no)->info2.palavra))){
+                maiorNo = inserirArv23(&((*no)->cen), informacao, promove, no);
+            }
+            else{
+                maiorNo = inserirArv23(&((*no)->dir), informacao, promove, no);
+            }
+        }
 
-            // Se o nó filho retornar um nó, ou seja, o nó foi quebrado, insere a palavra promovida no nó pai
-            if(maiorno != NULL){
-                if((*no)->ninfos == 1)
-                    *no = adicionaChave(*no, *promove, maiorno);
-                else
-                    maiorno = quebraNo(no, *promove, promove, maiorno);
-                    if(*pai == NULL){
-                        *no = criaNo(*promove, *no, maiorno);
-                        maiorno = NULL;
-                    }
+        if(maiorNo){
+            if((*no)->ninfos == 1){
+                *no = adicionaChave(*no, *promove, maiorNo);
+                maiorNo = NULL;
+            }
+            else{
+                maiorNo = quebraNo(no, *promove, &promove1, maiorNo);
+                if(*pai){
+                    *no = criaNo(promove1, *no, maiorNo);
+                    maiorNo = NULL;
+                }
             }
         }
     }
-
-    return maiorno;
 }
 
 /*-----------------------------------------------------------------------------------------------------*/
--- Q1/src/Q1.h
+++ Q1/src/Q1.h
@@ -5,26 +5,25 @@
 
 /* ESTRUTAS DAS ÁRVORES E DEMAIS */
 
-typedef struct arv23pt { // Árvore 2-3 para as palavras em português
-    char *word;
-    char *word2;
-    struct arv23pt *esq;
-    struct arv23pt *meio;
-    struct arv23pt *dir;
+typedef struct ingptbst{
+    char *palavra;
+    struct ingptbst *dir,*esq;
+} IngPTBST;
+
+typedef struct info {
+    char *palavra;
+    IngPTBST *versaoIng;
+    int unidade;
+} Info;
+
+typedef struct Arv23pt{
+    Info info1;
+    Info info2;
+    struct Arv23pt *cen;
+    struct Arv23pt *dir;
+    struct Arv24pt *esq;
     int ninfos;
-} Arv23pt;
-
-typedef struct eng {
-    char *word; // Palavra em inglês
-    Arv23pt *equipt; // Palavras equivalentes em português
-    int unidade; // Unidade a qual a palavra pertence
-} Eng;
-
-typedef struct engbst { // Árvore binária de busca para as palavras em inglês
-    Eng *info;
-    struct engbst *esq;
-    struct engbst *dir;
-} EngBST;
+} Arv23PT;
 
 /*-----------------------------------------------------------------------------------------------------*/
 
