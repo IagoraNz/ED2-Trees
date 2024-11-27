efd84cb46d6b0431ee7cc65c664fe72d85798b2e
FrancinaldoSB <francinaldosousabarbosa21@gmail.com>
2024-11-27 11:54:57 -0300

    Q2 - > Item i e II Completos, Item III Andamento.

---
+++ Q2/main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "src/Q2.h"
+
+void insere(ArvVP **raiz, InfoArvBin info){
+    int resultado;
+    resultado = inserirArvVP(raiz, info);
+    // Verificar o resultado
+    if (resultado == 1)
+    {
+        printf("Insercao bem-sucedida!\n");
+    }
+    else
+    {
+        printf("Erro ao inserir.\n");
+    }
+    (*raiz)->cor = 1;
+}
+
+void exibirArvVP(ArvVP *raiz){
+    if(raiz != NULL){
+        exibirArvVP(raiz->esq);
+        printf("%s\n", raiz->info.palavraPortugues);
+        printf("Cor: %d\n", raiz->cor);
+        exibirArvBin(raiz->info.arvBinIngles);
+        exibirArvVP(raiz->dir);
+    }
+}
+
+int main()
+{
+    ArvVP *raiz = NULL;
+    int quant = 0;
+
+    while (quant < 2)
+    {
+        InfoArvBin info;
+        info.palavraPortugues = (char *)malloc(50 * sizeof(char));
+        info.arvBinIngles = (IngPTBST *)malloc(sizeof(IngPTBST));
+        info.arvBinIngles->esq = NULL;
+        info.arvBinIngles->dir = NULL;
+        info.arvBinIngles->info.palavraIngles = (char *)malloc(50 * sizeof(char));
+
+        printf("Digite a palavra em portugues: ");
+        scanf("%s", info.palavraPortugues);
+        printf("Digite a palavra em ingles: ");
+        scanf("%s", info.arvBinIngles->info.palavraIngles);
+        printf("Digite a unidade: ");
+        scanf("%d", &info.arvBinIngles->info.unidade);
+
+        insere(&raiz, info);
+
+        quant++;
+    }
+    
+    printf("Cor da Raiz: %d\n", raiz->cor);
+
+    exibirArvVP(raiz);
+
+    removerArvVP(&raiz, "Bus", 1);
+
+    exibirArvVP(raiz);
+
+    return 0;
+}
---
+++ Q2/src/Q2.h
@@ -0,0 +1,52 @@
+#ifndef Q1_H
+#define Q1_H
+
+/*
+    Estruturas de Árvores Binarias e Árvore Vermelho e Preta
+*/
+
+typedef struct infoArvoreingptbst
+{
+  char *palavraIngles;
+  int unidade;
+} InfoArvoreIngPTBST;
+
+typedef struct ingptbst
+{
+  InfoArvoreIngPTBST info;
+  struct ingptbst *esq;
+  struct ingptbst *dir;
+} IngPTBST;
+
+typedef enum cor
+{
+  VERMELHO = 0,
+  PRETO = 1
+} Cor;
+
+typedef struct infoArvBin
+{
+  IngPTBST *arvBinIngles;
+  char *palavraPortugues;
+} InfoArvBin;
+
+typedef struct arvvp
+{
+  InfoArvBin info;
+
+  Cor cor;
+  struct arvvp *esq;
+  struct arvvp *dir;
+} ArvVP;
+
+/*
+=====================================================================================================
+*/
+
+int inserirArvVP(ArvVP **raiz, InfoArvBin info);
+
+void exibirArvBin(IngPTBST *raiz);
+
+void removerArvVP(ArvVP **raiz, const char *palavraEN, int unidade);
+
+#endif
